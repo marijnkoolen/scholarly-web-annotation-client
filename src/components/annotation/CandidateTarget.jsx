f1e85fc66e2312b8a1fa83a9ad5d8f3ee32dbcb1
Marijn Koolen <marijn.koolen@gmail.com>
2017-02-01 23:45:52 +0100

    Update text reference to new candidate target data structure

--- src/components/annotation/CandidateTarget.jsx
+++ src/components/annotation/CandidateTarget.jsx
@@ -13,14 +13,14 @@ class CandidateTarget extends React.Component {
     }
     render() {
         // TO DO: deal with elements that have multiple types
-        var text = this.props.candidate.text;
+        var text = this.props.candidate.params.text;
         if (text.length > 200) {
             text = text.substr(0,200) + "...";
         }
         return (
             <li
-				onClick={this.handleClick}
-				className="list-group-item candidate-target">
+                onClick={this.handleClick}
+                className="list-group-item candidate-target">
                 <span className="label label-info">{this.props.candidate.label}</span>:
                 <span>&nbsp;{text}</span>
             </li>
