0575dd677ca6cb3f57d081a9e73b56c0c1e8c783
Marijn Koolen <marijn.koolen@gmail.com>
2017-03-24 16:39:50 +0100

    Add script to load and configure annotation client

---
+++ public/js/load_annotator.js
@@ -0,0 +1,23 @@
+document.onreadystatechange = function () {
+	if (document.readyState === "complete") {
+		console.log("document ready!");
+		loadConfig((error, config) => {
+			rdfaAnnotator = new RDFaAnnotator.RDFaAnnotator(config);
+			rdfaAnnotator.addAnnotationViewer();
+		});
+	}
+}
+
+var loadConfig = function(callback) {
+    fetch("rdfa-annotation-config.json", {
+        method: "GET"
+    }).then(function(response) {
+        return response.json();
+    }).then(function(config) {
+        console.log("loading custom configuration");
+        return callback(null, config);
+    }).catch(function(error) {
+        return callback(error, null);
+    });
+}
+
