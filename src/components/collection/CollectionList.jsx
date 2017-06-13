e05b9f069d7e6cbacec198dfa2573481c7e66b6e
Marijn Koolen <marijn.koolen@gmail.com>
2017-06-13 12:26:29 +0200

    Add list update listeners

--- src/components/collection/CollectionList.jsx
+++ src/components/collection/CollectionList.jsx
@@ -4,47 +4,69 @@
 import React from 'react';
 import Collection from './Collection.jsx';
 import AnnotationUtil from './../../util/AnnotationUtil.js';
+import AppCollectionStore from './../../flux/CollectionStore';
 import AppAnnotationStore from './../../flux/AnnotationStore';
-import AnnotationActions from '../../flux/AnnotationActions.js';
+import CollectionActions from '../../flux/CollectionActions.js';
 
 export default class CollectionViewer extends React.Component {
-	constructor(props) {
-		super(props);
-		console.log("constructor done");
-		this.state = {
-			collections: []
-		}
-	}
+    constructor(props) {
+        super(props);
+        this.state = {
+            collections: [],
+            default: null
+        }
+    }
 
-	componentDidMount() {
-		console.log("collectionList mounted");
-		AppAnnotationStore.bind('load-collections', this.loadCollections.bind(this));
-	}
+    componentDidMount() {
+        AppCollectionStore.bind('loaded-collections', this.listCollections.bind(this));
+        AppCollectionStore.bind('saved-collection', this.getCollections.bind(this));
+        AppCollectionStore.bind('updated-collection', this.getCollections.bind(this));
+        AppAnnotationStore.bind('saved-annotation', this.addAnnotation.bind(this));
+        AppAnnotationStore.bind('deleted-annotation', this.getCollections.bind(this));
+        AppCollectionStore.bind('deleted-collection', this.getCollections.bind(this));
+        AppCollectionStore.bind('default-collection', this.setDefaultCollection.bind(this));
+        CollectionActions.getCollections();
+    }
 
-	loadCollections(collections) {
-		console.log(collections);
-		this.setState({collections: collections});
-	}
+    setDefaultCollection(collectionId) {
+        this.setState({default: collectionId});
+    }
 
-	render() {
-		let collections = this.state.collections.map((collection, index) => {
-			console.log(index);
-			let key = "collection-" + index;
-			return (
-				<div className="Collection" key={key}>
-					{collection.label}
-				</div>
-			)
-		})
-		return (
-			<div className="CollectionList">
-				<h3>Saved collections</h3>
-				<ul>
-					{collections}
-				</ul>
-			</div>
-		)
-	}
+    addAnnotation(annotation) {
+        if (this.state.default)
+            CollectionActions.addAnnotation(this.state.default, annotation.id);
+    }
+
+    getCollections(pageId) {
+        CollectionActions.getCollections();
+    }
+
+    listCollections(collections) {
+        this.setState({collections: collections});
+    }
+
+    render() {
+        let component = this;
+        let collections = component.state.collections.map((collection, index) => {
+            let key = "collection-" + index;
+            return (
+                <Collection
+                    collection={collection}
+                    currentUser={component.props.currentUser}
+                    key={key}
+                    defaultCollection={this.state.default}
+                />
+            )
+        })
+        return (
+            <div className="CollectionList">
+                <h3>Saved collections</h3>
+                <ul className="list-group">
+                    {collections}
+                </ul>
+            </div>
+        )
+    }
 
 }
 
