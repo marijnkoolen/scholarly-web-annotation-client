650557d9159c59530981f9dcc8fc48b15318c57b
Jaap Blom <jblom@beeldengeluid.nl>
2017-07-11 11:51:43 +0200

    added B&G mapper + test HTML + settings-example.py with example settings

--- resource_server.py
+++ resource_server.py
@@ -1,11 +1,25 @@
 import os
 from flask import Flask
+from flask import render_template
 from flask.ext.cors import CORS
+from BengResourceMapper import BengResourceMapper
+from settings import config
 
-app = Flask(__name__, static_url_path='', static_folder='public')
+app = Flask(__name__, static_url_path='', static_folder='public', template_folder='public')
 app.add_url_rule('/', 'root', lambda: app.send_static_file('testletter.html'))
+
 cors = CORS(app)
+
+@app.route('/beng-av-example')
+def bengavexample():
+	brm = BengResourceMapper(config)
+	data = brm.loadResource(config['BENG_RESOURCE_URL'])
+	return render_template('beng-av-example.html',
+		data=data,
+		series=data.get('series', None),
+		season=data.get('season', None),
+		program=data.get('program', None)
+	)
 
 if __name__ == '__main__':
     app.run(port=int(os.environ.get("PORT", 3002)), debug=True)
-
