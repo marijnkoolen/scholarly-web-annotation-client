d635a4c8317a3a0102912f3bac785c7fc6f39e9f
Marijn Koolen <marijn.koolen@gmail.com>
2018-01-29 11:52:13 +0100

    Use class instead of id for annotation viewer element

--- public/js/load_annotator.js
+++ public/js/load_annotator.js
@@ -2,13 +2,28 @@ document.onreadystatechange = function () {
     if (document.readyState === "complete") {
         loadConfig((error, config) => {
             annotator = new ScholarlyWebAnnotator.ScholarlyWebAnnotator(config);
-            var viewerElement = document.getElementById('annotation-viewer');
+            var viewerElement = document.getElementsByClassName('annotation-viewer')[0];
             annotator.addAnnotationClient(viewerElement);
         });
     }
 }
 
 var loadConfig = function(callback) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "config/van-gogh-annotation-config.json");
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === XMLHttpRequest.DONE) {
+            try {
+                var config = JSON.parse(xhr.responseText);
+                return callback(null, config);
+            } catch(error) {
+                console.log(error);
+                return callback(error, null);
+            }
+        }
+    }
+    xhr.send();
+    /*
     fetch("config/van-gogh-annotation-config.json", {
         method: "GET"
     }).then(function(response) {
@@ -19,5 +34,6 @@ var loadConfig = function(callback) {
     }).catch(function(error) {
         return callback(error, null);
     });
+    */
 }
 
