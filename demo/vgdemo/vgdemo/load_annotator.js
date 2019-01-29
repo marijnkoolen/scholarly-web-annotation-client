3ab7092c2cfc1e07f678d2ff30048082c4f0e384
IA\peterboo <peter.boot@huygens.knaw.nl>
2019-01-29 12:22:23 +0100

    Expanded vgdemo into three versions

--- demo/vgdemo/vgdemo/load_annotator.js
+++ demo/vgdemo/vgdemo/load_annotator.js
@@ -13,7 +13,7 @@
 }
 */
 
-/*document.onreadystatechange = function () { // wait till page is loaded
+document.onreadystatechange = function () { // wait till page is loaded
     if (document.readyState === "complete") {
         console.log("document ready!");
         loadConfig(function(error, config) { // load configuration file
@@ -41,37 +41,4 @@ var loadConfig = function(callback) {
     }).catch(function(error) {
         return callback(error, null);
     });
-}
-*/
-var configFile = "../annotator_config.json"; // location of the config file
-
-document.onreadystatechange = function () { // wait till page is loaded
-    if (document.readyState === "complete") {
-        loadConfig(function(error, config) { // load configuration file
-            if (error)
-                return null;
-            // instantiate, configure and insert client
-            annotator = new ScholarlyWebAnnotator.ScholarlyWebAnnotator(config);
-            var viewerElement = document.getElementById('swac-viewer');
-            annotator.addAnnotationClient(viewerElement);
-        });
-    }
-    console.log("annotator ready! (I hope)");
-}
-
-var loadConfig = function(callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", configFile);
-    xhr.onreadystatechange = function() {
-        if (xhr.readyState === XMLHttpRequest.DONE) {
-            try {
-                var config = JSON.parse(xhr.responseText);
-                return callback(null, config);
-            } catch(error) {
-                console.log(error);
-                return callback(error, null);
-            }
-        }
-    }
-    xhr.send();
 }
