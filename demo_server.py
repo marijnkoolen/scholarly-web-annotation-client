125777244c3f3cbda116e5269ac3e74cc6c26eb9
Marijn Koolen <marijn.koolen@gmail.com>
2019-02-08 12:46:33 +0100

    Update dependency packages, add demo server

--- demo_server.py
+++ demo_server.py
@@ -5,8 +5,8 @@ from flask import render_template
 from flask_cors import CORS
 from settings import config
 
-app = Flask(__name__, static_url_path='', static_folder='public', template_folder='public')
-app.add_url_rule('/', 'root', lambda: app.send_static_file('testletter.html'))
+app = Flask(__name__, static_url_path='', static_folder='demo', template_folder='demo')
+app.add_url_rule('/', 'root', lambda: app.send_static_file('vgdemo/original.html'))
 
 cors = CORS(app)
 
@@ -16,4 +16,4 @@ def scripts(filename):
     return send_from_directory('node_modules', filename)
 
 if __name__ == '__main__':
-    app.run(port=int(os.environ.get("PORT", 3001)), debug=True)
+    app.run(port=int(os.environ.get("PORT", 8080)), debug=True)
